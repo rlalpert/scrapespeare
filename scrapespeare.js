a3b439f8480e43b5857ca8fb0670e86e5d2b60c3
Rachel Alpert <ambitious.fishus@gmail.com>
2016-05-31 22:23:34 -0400

    Normalize words and fix conflicting obj.property names

--- scrapespeare.js
+++ scrapespeare.js
@@ -7,28 +7,29 @@ fs.readFile(file, 'utf8', (err, data) => {
   var fullText = data; // raw data string
   var removedPunctuation = fullText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\[\]?]/g,' '); // remove all punctuation except for apostrophes
   var removedNewlines = removedPunctuation.replace(/\r?\n|\r/g,' '); // remove newlines and replace them with ' '
-  var lowerCase = removedNewlines.toLowerCase(); // normalize everything tp lowercase
-  var words = lowerCase.split(' '); // split the string into an array of words
+  // var lowerCase = removedNewlines.toLowerCase(); // normalize everything tp lowercase
+  var words = removedNewlines.split(' '); // split the string into an array of words
 
 // remove any artifacts of splitting the string into an array
 for (var i = words.length-1; i--;){
   if (words[i] === '' ) words.splice(i, 1);
 }
 
-words.forEach((word) => {
-  word.trim(); // trim any whitespace, just in case
-});
+// words.forEach((word) => {
+//   word.trim(); // trim any whitespace, just in case
+// });
 
 // console.log(words.slice(250, 280)); // check our work
 
 var wordCounts = {}; // create empty object for adding wordcounts to
 
 for (var i = 0; i < words.length; i++) {
-  if (wordCounts.hasOwnProperty(words[i])) {  // if the word already exists in the wordCounts object
-    wordCounts[words[i]] += 1; // increment the count by one
+  let normalizedWord = words[i].toLowerCase().trim();
+  if (wordCounts.hasOwnProperty(normalizedWord)) {  // if the word already exists in the wordCounts object
+    wordCounts[normalizedWord] += 1; // increment the count by one
   }
   else {
-    wordCounts[words[i]] = 1; // if it doens't exist, add it to the object
+    wordCounts[normalizedWord] = 1; // if it doens't exist, add it to the object
   }
 }
 
@@ -36,6 +37,8 @@ for (var i = 0; i < words.length; i++) {
 fs.writeFile('wordcount.txt', JSON.stringify(wordCounts), 'utf8', (err) => {
   if (err) throw err;
   console.log('Finished creating word count.');
-}); 
+});
+
+// console.log(wordCounts.hamlet);
 
 });
