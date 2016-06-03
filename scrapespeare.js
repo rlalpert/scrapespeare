c5ded0288add6ac9e9400f2c05178660535a4ef5
Rachel Alpert <ambitious.fishus@gmail.com>
2016-06-03 19:59:49 -0400

    Fixed bug that caused freezing on large files

--- scrapespeare.js
+++ scrapespeare.js
@@ -9,18 +9,9 @@ fs.readFile(file, 'utf8', readFileCallback);
 function readFileCallback (err, data) {
   if (err) throw err;
   var fullText = data; // raw data string
-  console.log('Read data.');
-  var removedPunctuation = fullText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\[\]?]/g,' ');
-  console.log('Removed punctuation.'); // remove all punctuation except for apostrophes
+  var removedPunctuation = fullText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\[\]?]/g,' '); // remove all punctuation except for apostrophes
   var removedNewlines = removedPunctuation.replace(/\r?\n|\r/g,' '); // remove newlines and replace them with ' '
-  console.log('Removed newlines.>;');
   var words = removedNewlines.split(' '); // split the string into an array of words
-  console.log('Split words into array.');
-
-  // remove any artifacts of splitting the string into an array
-  for (var i = words.length-1; i--;){
-    if (words[i] === '' ) words.splice(i, 1);
-  }
 
   var wordCounts = {}; // create empty object for adding wordcounts to
   for (var i = 0; i < words.length; i++) {
@@ -32,6 +23,7 @@ function readFileCallback (err, data) {
       wordCounts[normalizedWord] = 1; // if it doens't exist, add it to the object
     }
   }
+
   // writes resulting object to a text file
   fs.writeFile(`${output}-wordcount.txt`, JSON.stringify(wordCounts), 'utf8', (err) => {
     if (err) throw err;
